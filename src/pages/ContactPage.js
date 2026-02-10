279ecab33c8d7d582d8905805d6eb978ed16e128
Mohammed Adam <mohammedadam275@gmail.com>
2026-02-10 15:26:08 -0800

    Restore ContactPage to render ContactForm

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,7 +1,9 @@
+import ContactForm from "../components/ContactForm";
+
 export default function ContactPage() {
   return (
-    <div style={{ padding: "40px", background: "yellow", color: "black" }}>
-      <h1>CONTACT PAGE IS RENDERING</h1>
+    <div>
+      <ContactForm />
     </div>
   );
 }
