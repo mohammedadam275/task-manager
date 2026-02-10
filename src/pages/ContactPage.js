d8747970eeaade6ddb6c77fff60fef7fd28b0e1a
Mohammed Adam <mohammedadam275@gmail.com>
2026-02-10 15:21:58 -0800

    Debug: force visible ContactPage render

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,7 @@
-import ContactForm from "../components/ContactForm";
-
 export default function ContactPage() {
   return (
-    <div>
-      <ContactForm />
+    <div style={{ padding: "40px", background: "yellow", color: "black" }}>
+      <h1>CONTACT PAGE IS RENDERING</h1>
     </div>
   );
 }
